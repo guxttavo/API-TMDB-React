f503149e08f1bbf438827899054f5c1125880db7
GusttavoBarbosa <gustavowbarbosas@gmail.com>
2022-10-03 18:08:45 -0300

    add search logic

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,33 @@
 import './Navbar.css';
-
+import { useState } from 'react';
 import { BiSearchAlt2 } from 'react-icons/bi';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const NavBar = () => {
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(search);
+
+    if (!search) return
+
+    navigate(`/search?q=${search}`)
+    setSearch("");
+  }
   return (
     <nav id="navbar">
       <h2>
         <Link to="/">MoviesLib</Link>
       </h2>
-      <form>
-        <input type="text" placeholder="Busque um filme" />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Busque um filme"
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
+        />
         <button type="submit">
           <BiSearchAlt2 />
         </button>
